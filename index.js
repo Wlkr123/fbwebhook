8df4c96b470aa99ec4ca95162a270f55a9505451
Tuguldur Ch <tuguldur.ch@nmma.co>
2024-07-08 18:40:59 +0800

    push

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+app.post('webhook/', function (req, res) {
+
+    const{body} = req
+    console.log({body})
+
+  res.send('Hello World');
+});
+
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 });
